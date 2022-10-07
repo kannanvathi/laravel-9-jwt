11b5621e4bbeb05a602cb68ec951ca2e5cc74127
kannanvathi <kannanvathi@gmail.com>
2022-10-07 13:40:19 +0530

    dependent file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+   return view('about');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/portfolio-details', function () {
+    return view('portfolio-details');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/blog-details', function () {
+    return view('blog-details');
+})->name('blog-details');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
